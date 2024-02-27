55b629429c7b882f807844dee27691acb76edf05
gentlemannerss <damonvanbergen@gmail.com>
2024-02-27 13:49:21 +0100

    CJS v1.3

--- src/index.cjs
+++ src/index.cjs
@@ -1,1 +1,6 @@
-import('./index.js').then(() => console.log('index.js loaded')).catch(e => console.log('Error loading index.js', e.message), e.stack);
+import('./index.js')
+    .then(() => console.log('index.js loaded'))
+    .catch(e => {
+        console.log('Error loading index.js', e.message);
+        console.error(e.stack);
+    });
